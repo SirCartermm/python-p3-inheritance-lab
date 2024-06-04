04ec355a69edddc02c7f90c4145fe5d8000df92e
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-04 16:03:44 +0300

    Modified student.py

--- lib/student.py
+++ lib/student.py
@@ -1,7 +1,8 @@
 class Student(User):
-    def __init__(self, first_name, last_name):
+    def _init__(self, first_name, last_name):
         super().__init__(first_name, last_name)
-        self.knowledge = []
+        self.knowledge =[]
 
-    def learn(self, new_knowledge):
-        self.knowledge.append(new_knowledge)
+    def learn(self, knowledge):
+        self.knowlegde.append(knowledge)
+        
